5f60a7dea22facf66ed68452ae03e523d62b5158
Fawzy Tat <fawzytat@FawzysMacStudio.lan>
2025-01-22 12:58:59 -0800

    testing deploy

--- workers/worker-a/src/index.ts
+++ workers/worker-a/src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World from worker a!');
+		return new Response('Hello World from worker a dude!');
 	},
 } satisfies ExportedHandler<Env>;
