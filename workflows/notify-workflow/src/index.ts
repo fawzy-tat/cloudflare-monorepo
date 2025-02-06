70fbbe52dbf778595f58543d8e35d0a4573657ea
fawzy-tat <ahmedfawzytat@gmail.com>
2025-02-05 23:34:58 -0800

    clean up

--- workflows/notify-workflow/src/index.ts
+++ workflows/notify-workflow/src/index.ts
@@ -45,33 +45,3 @@ export class NotifyWorkflow extends WorkflowEntrypoint<Env, Params> {
 		await step.sleep('wait on something', '1 minute');
 	}
 }
-// </docs-tag name="workflow-entrypoint">
-
-// <docs-tag name="workflows-fetch-handler">
-// export default {
-// 	async fetch(req: Request, env: Env): Promise<Response> {
-// 		let url = new URL(req.url);
-
-// 		if (url.pathname.startsWith('/favicon')) {
-// 			return Response.json({}, { status: 404 });
-// 		}
-
-// 		// Get the status of an existing instance, if provided
-// 		let id = url.searchParams.get('instanceId');
-// 		if (id) {
-// 			let instance = await env.MY_WORKFLOW.get(id);
-// 			return Response.json({
-// 				status: await instance.status(),
-// 			});
-// 		}
-
-// 		// Spawn a new instance and return the ID and status
-// 		let instance = await env.MY_WORKFLOW.create();
-// 		return Response.json({
-// 			id: instance.id,
-// 			details: await instance.status(),
-// 		});
-// 	},
-// };
-// </docs-tag name="workflows-fetch-handler">
-// </docs-tag name="full-workflow-example">
